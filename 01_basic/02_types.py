d18d243a2e14233257353084d55e5f09319a32cc
Guille <guillempaniagua@gmail.com>
2026-09-14 09:02:24 +0000

    Añadir ejercicio básico de tipos de datos

---
+++ 01_basic/02_types.py
@@ -0,0 +1,39 @@
+# Python tiene varios tipos de datos
+# int, float, str, bool, NoneType, list, tuple, dict, range, set 
+
+# Números enteros
+print("int:")
+print(type(10)) # Con type() mostraremos en consola el tipo de dato que es
+print(0)
+print(-5)
+print(238765786579876875646745) 
+print(238765786579876875646745 + 1) 
+
+# Números decimales
+print("float:")
+print(type(3.14))
+print(type(1.0))
+print(type(1e3))
+
+# Números complejos
+print("complex:")
+print(type(1 + 2j))
+
+# Cadenas de texto
+print("str:")
+print(type("Hola"))
+print(type(""))
+print(type("12345"))
+print(type("""
+Multilínea
+"""))
+
+# Boolean
+print("bool:")
+print(type(True))
+print(type(False))
+print(type(1<2))
+
+# NoneType para describir la ausencia de valor
+print("NoneType:")
+print(type(None))
