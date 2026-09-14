de09a895ae977afccb5f072227d2908c7e39a7b1
Guille <guillempaniagua@gmail.com>
2026-09-14 07:27:29 +0000

    Añadir ejercicio básico de print

---
+++ 01_basic/01_print.py
@@ -0,0 +1,16 @@
+# El módulo print() nos permite imprimir en consola
+print("Hola Mundo!")
+print('Hola Mundo!') #En Python da igual si ponemos "" o ''
+
+#Se imprimen tres valores y por defecto Python los separa con espacios en blanco
+print("Python", "es", "genial") 
+
+#Podemos elegir el elemento espaciador con el parámetro sep = ""
+print("Python", "es", "genial", sep = "-")
+
+#Podemos imprimir en una línea varios print con el parámetro end = ""
+print("Esto se imprime", end = " ")
+print("en una línea")
+
+
+
